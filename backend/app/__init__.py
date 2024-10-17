b5fac29786265b75441fe56cd5aecf21d0eab31f
Gabriel Dong <gabrielhdong@gmail.com>
2024-10-16 20:38:16 -0500

    cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS
 from .mongo import init_mongo
 from config import Config
-# from .firebase import get_firebase_db, printAuth
 
 from .routes.student_routes import student_bp
 from .routes.class_routes import class_bp
@@ -10,14 +9,9 @@ from .routes.grade_routes import grade_bp
 
 def create_app():
     app = Flask(__name__)
-
     app.config["MONGO_URI"] = Config.MONGO_URI
-    # app.config["SECRET_KEY"] = "your_secret_key_here"
 
     mongo = init_mongo(app)
-    # firebase_db = get_firebase_db()
-
-    # printAuth()
 
     try:
         mongo.db.command("ping")
@@ -25,10 +19,7 @@ def create_app():
     except Exception as e:
         print(f"Failed to connect to MongoDB: {e}")
 
-
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
-
-    # Import and register blueprints
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
     app.register_blueprint(student_bp, url_prefix='/student')
     app.register_blueprint(class_bp, url_prefix='/class')
@@ -36,9 +27,10 @@ def create_app():
 
     @app.after_request
     def after_request(response):
-        response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
+        response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
         response.headers.add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+        response.headers.add("Access-Control-Allow-Credentials", "true")
         return response
 
     return app
