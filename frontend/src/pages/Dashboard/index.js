b43bbf359b5e0d1b62ead54df601a73220e67e94
Elena Kim <elenakim@MacBook-Pro-413.local>
2024-11-12 02:24:11 -0600

    dashboard updated

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useAuth } from "../../stores/firebase"; // Make sure this path is correct
 import "./styles.css"; // Link to your CSS file for styling
 
 const Dashboard = () => {
+    const user = useAuth();
   // Hard-coded sample data
   const courses = [
     { code: 'CS 4278', professor: 'SINGH, V', grade: 'B' },
@@ -10,29 +12,39 @@ const Dashboard = () => {
   ];
 
   return (
-    <div className="container">
-      <h1>Student Dashboard</h1>
-      <div className="table-container">
-        <table className="dashboard-table">
-          <thead>
-            <tr>
-              <th>Course Name</th>
-              <th>Professor</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            {courses.map((course, index) => (
-              <tr key={index}>
-                <td>{course.code}</td>
-                <td>{course.professor}</td>
-                <td>{course.grade}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <>
+      {user ? (
+        user.emailVerified ? (
+          <div className="container">
+            <h1>Student Dashboard</h1>
+            <div className="table-container">
+              <table className="dashboard-table">
+                <thead>
+                  <tr>
+                    <th>Course Name</th>
+                    <th>Professor</th>
+                    <th>Grade</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {courses.map((course, index) => (
+                    <tr key={index}>
+                      <td>{course.code}</td>
+                      <td>{course.professor}</td>
+                      <td>{course.grade}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        ) : (
+          <h1>Please verify your email to access this page</h1>
+        )
+      ) : (
+        <h1>Please log in to access this page</h1>
+      )}
+    </>
   );
 };
 
